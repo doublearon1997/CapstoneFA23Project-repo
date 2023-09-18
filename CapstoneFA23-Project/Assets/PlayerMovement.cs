8a7d9ab3ae136f390d8c2e3e5a2113b37333bb40
AlexisMunoz4 <119436222+AlexisMunoz4@users.noreply.github.com>
2023-09-18 12:18:40 -0500

    Sample Level

--- CapstoneFA23-Project/Assets/PlayerMovement.cs
+++ CapstoneFA23-Project/Assets/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,23 +10,34 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Animator animator;
+    public Tilemap obstacles;
 
     Vector2 movement;
+    Vector3 moveToPosition;
 
     // Update is called once per frame
     void Update()
     {
+
+
+         
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+       
+
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
+          rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        
     }
 
 }
