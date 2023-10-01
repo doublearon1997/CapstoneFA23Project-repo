747242d69890983691d32886dfca1ea5e1294a96
reiffbraden <119434445+reiffbraden@users.noreply.github.com>
2023-10-01 00:56:05 -0500

    adding battle ui stuff

--- CapstoneFA23-Project/Assets/Scripts/BattleScripts/BattleSystem.cs
+++ CapstoneFA23-Project/Assets/Scripts/BattleScripts/BattleSystem.cs
@@ -20,12 +20,24 @@ public class BattleSystem : MonoBehaviour
     public Transform[] playerSpawns;
     public Transform[] enemySpawns;
 
-    public GameObject containerPlayer1;
+
     public GameObject panelPlayerActions;
 
-    public TMP_Text textPlayer1Name;
-    public TMP_Text textPlayer1ClassAndLevel;
-    public TMP_Text textPlayer1HP;
+
+    //Player Overview Panel Objects
+
+    public GameObject containerPlayer1;
+    public TMP_Text textPlayer1Name, textPlayer1ClassAndLevel, textPlayer1HP;
+
+    public GameObject containerPlayer2;
+    public TMP_Text textPlayer2Name, textPlayer2ClassAndLevel, textPlayer2HP;
+  
+    public GameObject containerPlayer3;
+    public TMP_Text textPlayer3Name, textPlayer3ClassAndLevel, textPlayer3HP;
+
+    public GameObject containerPlayer4;
+    public TMP_Text textPlayer4Name, textPlayer4ClassAndLevel, textPlayer4HP;
+
 
     List<PlayerBattler> playerBattlers;
     List<EnemyBattler> enemyBattlers;
@@ -42,8 +54,6 @@ public class BattleSystem : MonoBehaviour
 
         StartCoroutine(SetupBattle());
         StartCoroutine(DetermineNextBattler());
-
-
     }
 
     IEnumerator SetupBattle()
@@ -62,24 +72,58 @@ public class BattleSystem : MonoBehaviour
         yield return new WaitForSeconds(2f);
 
         state = BattleState.DETERMINENEXT;
-
     }
 
     public void SetupPartyOverviewPanel()
     {
         for(int i = 0; i < playerBattlers.Count; i++)
         {
+            PlayerBattler battler;
+
             if(i == 0)
             {
-                textPlayer1Name.text = playerBattlers[0].battlerName;
-                textPlayer1ClassAndLevel.text = " Lvl " + playerBattlers[0].level + " " + playerBattlers[0].playerClass;
-                textPlayer1HP.text = "" + playerBattlers[0].hp +"/" + playerBattlers[0].mhp;
+                battler = playerBattlers[0];
+
+                textPlayer1Name.text = battler.battlerName;
+                textPlayer1ClassAndLevel.text = " Lvl " + battler.level + " " + battler.playerClass;
+                textPlayer1HP.text = "" + battler.hp +"/" + battler.mhp;
 
                 containerPlayer1.SetActive(true);
-            }
+            }  
+            else if (i == 1)
+            {
+                battler = playerBattlers[1];
 
-            
-         
+                textPlayer2Name.text = battler.battlerName;
+                textPlayer2ClassAndLevel.text = " Lvl " + battler.level + " " + battler.playerClass;
+                textPlayer2HP.text = "" + battler.hp +"/" + battler.mhp;
+
+                containerPlayer2.SetActive(true);
+            }
+            else if (i == 2)
+            {
+                battler = playerBattlers[2];
+
+                textPlayer3Name.text = battler.battlerName;
+                textPlayer3ClassAndLevel.text = " Lvl " + battler.level + " " + battler.playerClass;
+                textPlayer3HP.text = "" + battler.hp +"/" + battler.mhp;
+
+                containerPlayer3.SetActive(true);
+            }
+            else if (i == 3)
+            {
+                battler = playerBattlers[3];
+
+                textPlayer4Name.text = battler.battlerName;
+                textPlayer4ClassAndLevel.text = " Lvl " + battler.level + " " + battler.playerClass;
+                textPlayer4HP.text = "" + battler.hp +"/" + battler.mhp;
+
+                containerPlayer4.SetActive(true);
+            }
+            else
+            {
+                UnityEngine.Debug.Log("Why is there more than 4 player battlers?");
+            }
         }
     }
 
